a85d129778fc9cd1685e8ef9f56352e1fc5ccfc8
Youmin Zheng <zheng.youmin@gmail.com>
2016-02-29 19:05:43 -0500

    Optimized the code and updated Records.txt file

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,38 @@
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        int total=0;    // Holds the total value
-        int counter=0;  // Counts how many records has been tallied
+    public static void main(String[] args)throws IOException {
+        //Opens the file and reads it.
+        String inputFile = "files/Records.txt";
+        File testFile = new File(inputFile);
+        readFile(inputFile);
+    }
+    public static void readFile(String inputFile)throws IOException{
+        String line = null;                     //Holds a line of integers from text file
+        int total=0;                            //Holds the total amount
+        int field1=0;                           //Holds field1 first integer in a line of text file
+        int field2=0;                           //Holds field2 second integer in a line of text file
+        int counter=0;                          //Holds the counter value
 
-        //Opens the file for reading
-        File file = new File("files/Records.txt");
-        Scanner inputFile = new Scanner(file);
+        //Reading a whole line of text from text file with BufferedReader
+        FileReader fileReader = null;
+        fileReader = new FileReader(inputFile);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-        //Read values from file line by line and tallies counter and total if it has a next line
-        while (inputFile.hasNext()){
-            int field1 = inputFile.nextInt();
-            int field2 = 0;
+
+        while((line = bufferedReader.readLine()) != null) {
+            String[] field = line.split(" ");   //Splits a line of text into an array using space a delimiter
+            field1 = Integer.valueOf(field[0]); //field1 is first number in array
+            field2 = Integer.valueOf(field[1]); //field2 is second number in array
+
             if(field1 == 0){
                 total = total + field1;
                 counter++;
-                field1 = inputFile.nextInt();
             }
             else{
-                field2 = inputFile.nextInt();
                 if(field2 == 0){
-                    System.out.println("The total is: " + total+"." + " The number of records is: " + counter);
+                    System.out.println("The total is: " + total+"." + " The number of counters is: " + counter);
                     counter=0;
                 }
                 else{
@@ -34,26 +42,30 @@ public class Main {
             //Prints "End Record"
             System.out.println("End Record");
         }
-        //Prints the record counter
-        System.out.println("The number of records is: " + counter);
+        //Prints the counter amount.
+        System.out.println("The number of counters is: " + counter);
 
-        //Close the file.
-        inputFile.close();
+        //Closes the file.
+        bufferedReader.close();
     }
 
     public int unit(int field1, int field2){
         int total=0;
+        int counter=0;
         if(field1 == 0){
-            total = total + field1;
+            total += field1;
+            counter++;
         }
         else{
             if(field2 == 0){
-                System.out.println("The total is: " + total+".");
+                System.out.println("The total is: " + total+"." + " The number of counters is: " + counter);
+                counter=0;
             }
             else{
-                total = total - field2;
+                total -= field2;
             }
         }
         return total;
     }
 }
+
